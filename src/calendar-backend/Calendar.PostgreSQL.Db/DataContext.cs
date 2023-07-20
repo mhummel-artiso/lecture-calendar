2a1943c2c5a17ce76a6d6e2c12adfeaed4a7d427
MHummel <mhummel@artiso.com>
2023-07-20 23:31:47 +0200

    rename calendar.maria.db to calendar.posgress.db for secure key handling

---
+++ src/calendar-backend/Calendar.PostgreSQL.Db/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calendar.PostgreSQL.Db;
+
+public class PersistKeyContext : DbContext, IDataProtectionKeyContext
+{
+    public PersistKeyContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+}
