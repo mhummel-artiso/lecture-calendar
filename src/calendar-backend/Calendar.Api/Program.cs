80772664e3fd52a546b6b84af3d2eaaf7ba47672
MHummel <mhummel@artiso.com>
2023-07-20 23:43:49 +0200

    Modify Program.cs (see description)
    add regions for the different configurations
    add new environment configurations
    use validator method bevor use envs
    add logger (Serilog)
    configure logger
    update mongo configuration name
    add PostgreSql datacontext
    add AddDataProtection with PostgreSql
    start configure jwt and authentication provider
    Add authorization (TODO configure)
    group service registration
    configure swagger gen with ConfigureSwaggerGenOptions
    configure swagger ui for oAuht
    add SerilogRequestLogging
    add Authentication to app
    add a try catch to get all ArgumentException from the validate methods

--- src/calendar-backend/Calendar.Api/Program.cs
+++ src/calendar-backend/Calendar.Api/Program.cs
@@ -1,65 +1,174 @@
+using Amazon.Runtime.Internal.Transform;
 using Calendar.Api.Configurations;
 using Calendar.Api.Services;
 using Calendar.Api.Services.Interfaces;
-using Microsoft.OpenApi.Models;
+using Calendar.PostgreSQL.Db;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using Serilog;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var configuration = builder.Configuration;
-builder.Services.Configure<MongoDbEnvironmentConfiguration>(configuration);
-var environmentConfig = configuration.Get<MongoDbEnvironmentConfiguration>();
-
-// Env validation
-if (environmentConfig == null) throw new ArgumentNullException(nameof(environmentConfig));
-if (environmentConfig.MONGODB_CONNECTIONSTRING == null) throw new ArgumentNullException(nameof(environmentConfig.MONGODB_CONNECTIONSTRING));
-
-// Mongo configuration
-builder.Services.AddSingleton<IMongoClient>(x => new MongoClient(environmentConfig.MONGODB_CONNECTIONSTRING));
-builder.Services.AddScoped<IMongoDatabase>(x =>
+try
 {
-    var client = x.GetRequiredService<IMongoClient>();
-    return client.GetDatabase(environmentConfig.MONGODB_DB_NAME);
-});
 
-// Add services to the container.
-builder.Services.AddScoped<ICalendarService, CalendarService>();
-builder.Services.AddScoped<ILectureService, LectureService>();
-builder.Services.AddScoped<IEventService, EventService>();
+    #region Configuration
 
-// Add automapper
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    var configuration = builder.Configuration;
+    builder.Services.AddOptions();
+    builder.Services.Configure<MongoDbEnvironmentConfiguration>(configuration);
+    builder.Services.Configure<PostgreSqlEnvironmentConfiguration>(configuration);
+    builder.Services.Configure<JwtEnvironmentConfiguration>(configuration);
+    builder.Services.Configure<OidcEnvironmentConfiguration>(configuration);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo()
+    #endregion
+
+    #region Logger
+
+    builder.Host.UseSerilog((ctx, config) =>
+        config
+            .ReadFrom
+            .Configuration(configuration)
+            .Enrich
+            .FromLogContext()
+            .WriteTo
+            .Console()
+    );
+
+    //builder.Services.AddSerilog();
+
+    #endregion
+
+    #region Mongo db
+
+    var mongoConfig = configuration.Get<MongoDbEnvironmentConfiguration>()?.Validate();
+    ArgumentNullException.ThrowIfNull(mongoConfig);
+    builder.Services.AddSingleton<IMongoClient>(x =>
     {
-        Version = "v1",
-        Title = "Calendar Api",
-        Description = "Api for calender app"
+        return new MongoClient(mongoConfig.MONGODB_SERVER);
     });
-});
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
+    builder.Services.AddScoped<IMongoDatabase>(x =>
     {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        var client = x.GetRequiredService<IMongoClient>();
+        return client.GetDatabase(mongoConfig.MONGODB_DB_NAME);
     });
+
+    #endregion
+
+    #region PostgreSQL db
+
+    // https://learn.microsoft.com/en-us/aspnet/core/security/data-protection/configuration/overview?view=aspnetcore-3.1
+    builder.Services.AddDbContext<PersistKeyContext>(options =>
+    {
+        var postgresqlConfig = configuration.Get<PostgreSqlEnvironmentConfiguration>()?.Validate();
+        ArgumentNullException.ThrowIfNull(postgresqlConfig);
+        var str = postgresqlConfig.GetConnectionString();
+        options.UseNpgsql(str);
+    });
+
+    builder.Services.AddDataProtection().PersistKeysToDbContext<PersistKeyContext>();
+
+    #endregion
+
+    #region Authentication and authorization
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            var isDebug = builder.Environment.IsDevelopment();
+            var jwtConfig = configuration.Get<JwtEnvironmentConfiguration>()?.Validate();
+            ArgumentNullException.ThrowIfNull(jwtConfig);
+            // see: https://dev.to/kayesislam/integrating-openid-connect-to-your-application-stack-25ch
+            options.IncludeErrorDetails = isDebug;
+            options.RequireHttpsMetadata = !isDebug;
+            options.MetadataAddress = jwtConfig.JWT_METADATA_ADDRESS;
+            options.Authority = jwtConfig.JWT_AUTHORITY;
+            options.TokenValidationParameters = new()
+            {
+                ValidAudience = jwtConfig.JWT_AUDIENCE,
+                // ValidIssuer = jwtConfig.JWT_ISSUER,
+                //// IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.JWT_KEY)),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = false,
+                ValidateIssuerSigningKey = true
+            };
+        });
+
+    // TODO configure correctly
+    builder.Services.AddAuthorization(options =>
+    {
+        options.DefaultPolicy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .RequireClaim("roles")
+            .Build();
+    });
+
+    #endregion
+
+    #region Services and automapper
+
+    builder.Services
+        .AddScoped<ICalendarService, CalendarService>()
+        .AddScoped<ILectureService, LectureService>()
+        .AddScoped<IEventService, EventService>()
+        .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGenOptions>()
+        .AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+    #endregion
+
+    #region Controller
+
+    builder.Services.AddControllers();
+    builder.Services.AddHealthChecks();
+
+    #endregion
+
+    #region Swagger
+
+    var swaggerConfig = configuration.Get<OidcEnvironmentConfiguration>()?.Validate();
+    ArgumentNullException.ThrowIfNull(swaggerConfig);
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(); // configured by configurator (IConfigureOptions<SwaggerGenOptions>)
+
+    #endregion
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        // https://www.camiloterevinto.com/post/oauth-pkce-flow-for-asp-net-core-with-swagger
+        app.UseSwaggerUI(options =>
+        {
+            options.OAuthClientId("calendar-api-swagger");
+            options.OAuthClientSecret(swaggerConfig.OIDC_SWAGGER_CLIENT_SECRET);
+            options.OAuthScopes("profile", "openid", "roles");
+            options.OAuth2RedirectUrl(swaggerConfig.OIDC_SWAGGER_REDIRECT_URL);
+            options.OAuthUsePkce();
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        });
+    }
+
+    app.UseSerilogRequestLogging();
+    // app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (ArgumentException ex)
+{
+    Log.Logger.Error(ex, "Error on startup");
+    throw new ArgumentException("Error on startup", ex);
+}
