aaa2527108a355144deb5d46e194c91d1f563e9d
MHummel <mhummel@artiso.com>
2023-07-21 10:55:36 +0200

    remove unnesasary check

--- src/calendar-client/src/hooks/useEnvironment.tsx
+++ src/calendar-client/src/hooks/useEnvironment.tsx
@@ -5,9 +5,6 @@ export interface EnvConfig {
     BASE_URL:string;
 }
 export const useEnvironment = () => {
-    if(!import.meta.env.VITE_API_URL) throw new Error('VITE_API_URL not set')
-    if(!import.meta.env.VITE_OIDC_AUTHORITY) throw new Error('VITE_OIDC_AUTHORITY not set')
-    if(!import.meta.env.VITE_OIDC_CLIENT_SECRET) throw new Error('VITE_OIDC_CLIENT_SECRET not set')
     return {
         VITE_API_URL: import.meta.env.VITE_API_URL as string,
         VITE_OIDC_AUTHORITY: import.meta.env.VITE_OIDC_AUTHORITY as string,
