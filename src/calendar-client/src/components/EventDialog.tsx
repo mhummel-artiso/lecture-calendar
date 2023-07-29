1773ddece5b2ad79ff57d6537799d96f9b39ce6c
[Samuel Kraut] <[skraut@artiso.com]>
2023-07-29 11:21:03 +0200

    add refs for submit

--- src/calendar-client/src/components/EventDialog.tsx
+++ src/calendar-client/src/components/EventDialog.tsx
@@ -8,12 +8,13 @@ import {
     Stack,
     TextField,
 } from '@mui/material'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { TimePicker } from '@mui/x-date-pickers/TimePicker'
 import { fetchCalendars } from '../services/CalendarService'
 import { useQuery } from '@tanstack/react-query'
 import { fetchLectures } from '../services/LectureService'
+import { DateTimePicker } from '@mui/x-date-pickers'
 
 interface Props {
     isDialogOpen: boolean
@@ -40,6 +41,12 @@ export const EventDialog = ({ isDialogOpen, handleDialogClose }: Props) => {
     const [selectedValue, setSelectedValue] = useState(serialList[0].value)
     const [selectedCalendarId, setSelectedCalendarId] = React.useState('') // Change to use course that Person is currently on its calendar
     const [selectedLectureId, setSelectedLectureId] = React.useState('')
+
+    const eventStart = useRef<Date>()
+    const eventEnd = useRef<Date>()
+    const eventLocation = useRef<string>()
+    const eventDescription = useRef<string>()
+    const eventSerieEnd = useRef<Date>()
 
     const handleClose = () => {
         setSelectedCalendarId('')
@@ -79,9 +86,14 @@ export const EventDialog = ({ isDialogOpen, handleDialogClose }: Props) => {
                         ))}
                     </TextField>
                     <Stack direction="row" spacing={2} sx={{ mt: 1, mb: 1 }}>
-                        <DatePicker label="Tag" />
-                        <TimePicker label="Beginn" />
-                        <TimePicker label="Ende" />
+                        <DateTimePicker
+                            onChange={(value) => (eventStart.current = value)}
+                            label="Start"
+                        />
+                        <DateTimePicker
+                            onChange={(value) => (eventEnd.current = value)}
+                            label="Ende"
+                        />
                     </Stack>
                     <Stack direction="row">
                         <TextField
@@ -104,19 +116,19 @@ export const EventDialog = ({ isDialogOpen, handleDialogClose }: Props) => {
                             label="Serienende"
                             sx={{ ml: 2, mt: 1, mb: 1 }}
                             disabled={selectedValue === serialList[0].value}
+                            onChange={(value) =>
+                                (eventSerieEnd.current = value)
+                            }
                         />
                     </Stack>
-                    <TextField
-                        margin="dense"
-                        id="dozent"
-                        type="text"
-                        label="Dozent"
-                    />
                     <TextField
                         margin="dense"
                         id="name"
                         label="Vorlesungsort"
                         type="text"
+                        onChange={(value) =>
+                            (eventLocation.current = value.target.value)
+                        }
                     />
                     <TextField
                         multiline
@@ -125,17 +137,26 @@ export const EventDialog = ({ isDialogOpen, handleDialogClose }: Props) => {
                         type="text"
                         label="Zusätzliche Infos"
                         maxRows={4}
+                        onChange={(value) =>
+                            (eventDescription.current = value.target.value)
+                        }
                     />
                 </Stack>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose}>Abbrechen</Button>
                 <Button
+                    type="submit"
                     onClick={() =>
                         console.log(
                             selectedCalendarId,
                             selectedLectureId,
-                            selectedValue
+                            selectedValue,
+                            eventLocation.current,
+                            eventDescription.current,
+                            eventStart.current?.toISOString(),
+                            eventEnd.current?.toISOString(),
+                            eventSerieEnd.current?.toISOString()
                         )
                     }
                 >
