5bf848335a6a20cb0768b869927669e0b5deb2c0
Adamicki, Karolina - TIN2021 <adamickik.tin21@student.dhbw-heidenheim.de>
2023-08-06 15:45:13 +0200

    changed css of Hello Page

--- src/calendar-client/src/components/Pages/HelloPage.tsx
+++ src/calendar-client/src/components/Pages/HelloPage.tsx
@@ -4,18 +4,20 @@ import { Grid, Typography } from "@mui/material";
 
 export const HelloPage=()=>
 {
-
     return (
         <>
             <Grid container
                   direction="column"
-                  spacing={4}
                   justifyContent="center"
-                  alignItems="center" sx={{minHeight:"50%"}}>
-            <Typography variant="h3">
-                Deine Kurse
-            </Typography>
-            <AvailableCalendarsList />
+                  alignItems="center" 
+                  sx={{
+                    minHeight: '90vh',
+                    display: 'flex',
+                  }}>
+                <Typography variant="h3">
+                    Deine Kurse
+                </Typography>
+                <AvailableCalendarsList />
             </Grid>
         </>
     )
