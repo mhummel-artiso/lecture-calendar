e822d49005be684a370f6169acb7fbb3e21ba2b9
MHummel <mhummel@artiso.com>
2023-09-14 09:31:17 +0200

    import mongo backup at starup

--- config/mongodb/init/mongo-init.sh
+++ config/mongodb/init/mongo-init.sh
@@ -1,9 +1,12 @@
 set -e
 
 # Here comes initial data from mongo
-mongorestore --host mongodb --port 27017 --username root --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --drop --gzip --db lecture-calendar --archive=/docker-entrypoint-initdb.d/lecture-calendar.gz
-mongosh <<EOF
-use lecture-calendar;
-db.createCollection('events')
-db.createCollection('lectures')
-EOF
+mongorestore \
+  --host mongodb \
+  --port 27017 \
+  --username root \
+  --password $MONGO_INITDB_ROOT_PASSWORD \
+  --authenticationDatabase admin \
+  --drop --gzip \
+  --db lecture-calendar \
+  --archive=/docker-entrypoint-initdb.d/lecture-calendar.gz
