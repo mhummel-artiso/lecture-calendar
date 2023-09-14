b8da246976fbf49712b801eeb50c0fe0df3327fd
MHummel <mhummel@artiso.com>
2023-09-14 15:36:29 +0200

    configure app to run in demo mode

--- config/mongodb/init/mongo_restore.sh
+++ config/mongodb/init/mongo_restore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Restore from dump
 
-mongorestore --host mongodb --port 27017 --username root --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --drop --gzip --db lecture-calendar --archive=/backup_data/lecture-calendar.gz
+docker exec -i lecture-calendar-mongodb-1 /usr/bin/mongorestore --host mongodb --port 27017 --username root --password '74RuqICjDPQEFREmhIFaqRf6H' --authenticationDatabase admin --drop --gzip --db lecture-calendar --archive=/backup_data/lecture-calendar.gz
