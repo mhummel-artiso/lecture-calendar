aaffb4065f3aea9f4d9893879323605c6ccc9285
[Samuel Kraut] <[skraut@artiso.com]>
2023-09-26 21:16:44 +0200

    add keycloak service comments

--- src/calendar-backend/Calendar.Api/Services/Interfaces/IKeycloakService.cs
+++ src/calendar-backend/Calendar.Api/Services/Interfaces/IKeycloakService.cs
@@ -4,9 +4,22 @@ namespace Calendar.Api.Services.Interfaces;
 
 public interface IKeycloakService
 {
+    /// <summary>
+    /// Get assigned Calendars
+    /// </summary>
+    /// <param name="userId">User ID</param>
+    /// <returns>A collection of <see cref="string"/></returns>
     Task<IEnumerable<string>?> GetAssignedCalendarsByUserAsync(string userId);
 
+    /// <summary>
+    /// Get instructors from Keycloak
+    /// </summary>
+    /// <returns>A nullable collection of <see cref="InstructorDTO"/></returns>
     Task<IEnumerable<InstructorDTO>?> GetInstructorsAsync();
 
+    /// <summary>
+    /// Get all calendar groups from keycloak
+    /// </summary>
+    /// <returns>A nullable collection of <see cref="UserCalendarKeycloakDTO"/></returns>
     Task<IEnumerable<UserCalendarKeycloakDTO>?> GetAllCalendarsAsync();
 }
