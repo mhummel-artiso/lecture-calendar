fd18b85a7218130cabb5a151711750866ab99b40
SamuelKraut <97592367+SamuelKraut@users.noreply.github.com>
2023-09-27 11:00:09 +0200

    Api/comments (#30)
    * add lecture comments
    
    * delete unused key generator
    
    * add keycloak service comments
    
    * update calendar service comments
    
    * add event service comments
    
    ---------
    
    Co-authored-by: [Samuel Kraut] <[skraut@artiso.com]>

--- src/calendar-backend/Calendar.Api/Services/Interfaces/ICalendarService.cs
+++ src/calendar-backend/Calendar.Api/Services/Interfaces/ICalendarService.cs
@@ -1,19 +1,51 @@
-﻿using Calendar.Api.Models;
-using Calendar.Mongo.Db.Models;
+﻿using Calendar.Mongo.Db.Models;
 
 namespace Calendar.Api.Services.Interfaces;
 
 public interface ICalendarService
 {
+    /// <summary>
+    /// Get calendars from database searched by names
+    /// </summary>
+    /// <param name="names">names of calendars which are searched for</param>
+    /// <param name="includeEvents">if calendars should contains events</param>
+    /// <returns>A collection of<see cref="UserCalendar"/></returns>
     Task<IEnumerable<UserCalendar>> GetCalendarsByNamesAsync(IEnumerable<string> names, bool includeEvents = false);
+
+    /// <summary>
+    /// Get a calendar from database
+    /// </summary>
+    /// <param name="calendarId">calendar ID</param>
+    /// <param name="includeEvents">return calendar with or without events</param>
+    /// <returns>A nullable <see cref="UserCalendar"/></returns>
     Task<UserCalendar?> GetCalendarByIdAsync(string calendarId,bool includeEvents = false);
-    
+
+    /// <summary>
+    /// Add a new calendar to database
+    /// </summary>
+    /// <param name="calendar">calendar to add</param>
+    /// <returns><see cref="UserCalendar"/></returns>
+    /// <exception cref="KeyNotFoundException">There is already an calendar with the same name.</exception>
     Task<UserCalendar> AddCalendarAsync(UserCalendar calendar);
 
-    // Warning: Be careful not to delete events.
+    /// <summary>
+    /// Update a specific calendar
+    /// </summary>
+    /// <param name="calendarId">calendar id</param>
+    /// <param name="updateCalendar">calendar with new states</param>
+    /// <returns>A nullable <see cref="UserCalendar"/></returns>
     Task<UserCalendar?> UpdateCalendarAsync(string calendarId, UserCalendar updateCalendar);
 
+    /// <summary>
+    /// Delete a calendar
+    /// </summary>
+    /// <param name="calendarId">calendar id</param>
+    /// <returns><see cref="bool"/></returns>
     Task<bool> DeleteCalendarByIdAsync(string calendarId);
-    
+
+    /// <summary>
+    /// Get all calendar form database without there events
+    /// </summary>
+    /// <returns>A collection of<see cref="UserCalendar"/></returns>
     Task<IEnumerable<UserCalendar>> GetCalendars();
 }
--- src/calendar-backend/Calendar.Api/Services/Interfaces/IEventService.cs
+++ src/calendar-backend/Calendar.Api/Services/Interfaces/IEventService.cs
@@ -1,21 +1,91 @@
 ﻿using Calendar.Api.Models;
 using Calendar.Mongo.Db.Models;
-using MongoDB.Bson;
+using Calendar.Api.Exceptions;
 
 namespace Calendar.Api.Services.Interfaces
 {
     public interface IEventService
     {
-        Task<CalendarEvent?> GetEventAsync(string calendarId, string eventId);
+        /// <summary>
+        /// Get event
+        /// </summary>
+        /// <param name="calendarId">calendar id</param>
+        /// <param name="eventId">event id</param>
+        /// <returns><see cref="CalendarEvent"/></returns>
+        /// <exception cref="KeyNotFoundException">There is no calendar with the calendar id.</exception>
+        /// <exception cref="KeyNotFoundException">There is no event with the event id.</exception>
+        Task<CalendarEvent> GetEventAsync(string calendarId, string eventId);
+
+        /// <summary>
+        /// Get events from calendar from specific day, week or month
+        /// </summary>
+        /// <param name="calendarId">calendar id</param>
+        /// <param name="viewType">should it a day, week or month</param>
+        /// <param name="date">date</param>
+        /// <returns>A nullable collection of <see cref="CalendarEvent"/></returns>
+        /// <exception cref="ArgumentOutOfRangeException">viewType not found</exception>
         Task<IEnumerable<CalendarEvent>?> GetEventsAsync(string calendarId, ViewType viewType, DateTimeOffset date);
+
+        /// <summary>
+        /// Get all events from calendar
+        /// </summary>
+        /// <param name="calendarId">calendar id</param>
+        /// <returns>A collection of <see cref="CalendarEvent"/></returns>
+        /// <exception cref="KeyNotFoundException">There is no calendar with the calendar id.</exception>
         Task<IEnumerable<CalendarEvent>> GetAllEventsFromCalendarAsync(string calendarId);
 
+        /// <summary>
+        /// Add a new event or series
+        /// </summary>
+        /// <param name="calendarId">calendar id</param>
+        /// <param name="calendarEvent">event to create event/series</param>
+        /// <returns>A nullable collection of <see cref="CalendarEvent"/></returns>
+        /// <exception cref="ArgumentNullException">invalid time inputs</exception>
+        /// <exception cref="ArgumentNullException">calendar id is null</exception>
         Task<IEnumerable<CalendarEvent>?> AddEventAsync(string calendarId, CalendarEvent calendarEvent);
 
-        Task<CalendarEvent> UpdateEventAsync(string calendarId, CalendarEvent lectureEvent);
+        /// <summary>
+        /// Update a specific event
+        /// </summary>
+        /// <param name="calendarId">calendar id</param>
+        /// <param name="calendarEvent">event to update</param>
+        /// <returns><see cref="CalendarEvent"/></returns>
+        /// <exception cref="KeyNotFoundException">There is no calendar with the calendar id.</exception>
+        /// <exception cref="KeyNotFoundException">There is no event with the event id.</exception>
+        /// <exception cref="ConflictException{T}">lecture.LastUpdateDate is not the same as in the database.</exception>
+        /// <exception cref="ArgumentNullException">calendar id is null</exception>
+        /// <exception cref="Exception">problem while updating</exception>
+        Task<CalendarEvent> UpdateEventAsync(string calendarId, CalendarEvent calendarEvent);
+
+        /// <summary>
+        /// Update a event series
+        /// </summary>
+        /// <param name="calendarId">calendar id</param>
+        /// <param name="calendarEvent">event to update</param>
+        /// <returns>A collection of <see cref="CalendarEvent"/></returns>
+        /// <exception cref="KeyNotFoundException">There is no calendar with the calendar id.</exception>
+        /// <exception cref="KeyNotFoundException">There is no serie with the serie id.</exception>
+        /// <exception cref="ConflictException{T}">lecture.LastUpdateDate is not the same as in the database.</exception>
+        /// <exception cref="ArgumentNullException">calendar id is null</exception>
+        /// <exception cref="Exception">problem while updating</exception>
         Task<IEnumerable<CalendarEvent>> UpdateEventSeriesAsync(string calendarId, CalendarEvent calendarEvent);
 
+        /// <summary>
+        /// Delete a event
+        /// </summary>
+        /// <param name="calendarId">calendar id</param>
+        /// <param name="eventId">event id</param>
+        /// <returns><see cref="bool"/></returns>
+        /// <exception cref="KeyNotFoundException">There is no calendar with the calendar id.</exception>
         Task<bool> DeleteEventByIdAsync(string calendarId, string eventId);
+
+        /// <summary>
+        /// Delete series (all events)
+        /// </summary>
+        /// <param name="calendarId">calendar id</param>
+        /// <param name="seriesId">series id</param>
+        /// <returns><see cref="bool"/></returns>
+        /// <exception cref="KeyNotFoundException">There is no calendar with the calendar id.</exception>
         Task<bool> DeleteEventSeriesByIdAsync(string calendarId, string seriesId);
     }
 }
